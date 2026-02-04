947c0d8713c55e091197e77878ad370675da2e2a
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-02-04 21:25:47 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+
+  backend "azurerm" {
+    storage_account_name = "tfmoduleswhatislavxsa"
+    container_name       = "tfstate"
+    resource_group_name  = "tf-modules-whatislavx-rg"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
